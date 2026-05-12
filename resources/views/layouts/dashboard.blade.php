2e3059985254331bede80d33d618845838b1268c
nirbhaydhaked1 <nirbhaydhaked@gmail.com>
2026-05-12 18:53:09 +0530

    fix syntex error

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'CRM'))</title>
-    <link rel="icon" type="image/svg+xml" href="{{ asset(public/'public/favicon.svg') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('public/favicon.svg') }}">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-lite.min.css" rel="stylesheet">
